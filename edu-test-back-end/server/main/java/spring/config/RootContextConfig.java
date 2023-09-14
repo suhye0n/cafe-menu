b8a0fdc6c5754bb44da7e2be3a0654233488b7e2
suhye0n <contact.suhyeon@gmail.com>
2023-09-14 11:25:19 +0900

    스타일 수정
    스타일 수정

--- edu-test-back-end/server/main/java/spring/config/RootContextConfig.java
+++ edu-test-back-end/server/main/java/spring/config/RootContextConfig.java
@@ -21,16 +21,9 @@ import java.util.TimeZone;
 @Configuration
 public class RootContextConfig {
 
-    /**
-     * HikariCP(DBCP) Config(설정 세팅) 객체 Bean 설정
-     *
-     * @author 하석형
-     * @since 2023.08.21
-     */
     @Bean(name = "mainHikariConfig")
     public HikariConfig getMainHikariConfig () {
         HikariConfig hikariConfig = new HikariConfig();
-        /*MariaDB*/
         hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
         hikariConfig.setJdbcUrl("jdbc:mariadb://localhost:3306/test");
         hikariConfig.setUsername("root");
@@ -38,46 +31,29 @@ public class RootContextConfig {
         return hikariConfig;
     }
 
-    /**
-     * HikariCP(DBCP) 객체 Bean 설정
-     *
-     * @author 하석형
-     * @since 2023.08.21
-     */
     @Bean(name = "mainHikariDataSource")
     public HikariDataSource getMainHikariDataSource () {
         HikariDataSource hikariDataSource = new HikariDataSource(getMainHikariConfig());
         return hikariDataSource;
     }
 
-    //Mybatis 동작 설정
-    //mybatis-config.xml 작성 대신 Class 활용
     private class Configuration extends org.apache.ibatis.session.Configuration {
         private Configuration() {
             super();
-            super.setCacheEnabled(true);//mapper 캐시 전역 사용여부
-            super.setLazyLoadingEnabled(false);//mybatis 지연 로딩 사용여부
-            super.setMultipleResultSetsEnabled(true);//한개의 구문에서 여러개의 ResultSet 허용 여부
-            super.setUseColumnLabel(true);//컬럼명 대신 컬럼라벨 사용 여부
-            super.setUseGeneratedKeys(false);//키 자동 생성
+            super.setCacheEnabled(true);
+            super.setLazyLoadingEnabled(false);
+            super.setMultipleResultSetsEnabled(true);
+            super.setUseColumnLabel(true);
+            super.setUseGeneratedKeys(false);
             super.setDefaultExecutorType(ExecutorType.SIMPLE);
             super.setDefaultStatementTimeout(25000);
-            super.setCallSettersOnNulls(true);//조회 조회시, null값 무조건 허용
+            super.setCallSettersOnNulls(true);
 
-            //Mybatis SQL 작성시, 자주 활용하는 Class 별칭 세팅
             TypeAliasRegistry typeAliasRegistry = super.getTypeAliasRegistry();
         }
     }
-    //Mybatis 동작 설정 객체 생성
     private Configuration configuration = new Configuration();
 
-    /**
-     * Main
-     * Mapper : SQL이 작성된 문서와, 작성된 SQL을 실행시킬 class(Mapper)를 매핑 시켜주기 위한 객체 Been 설정 (Main)
-     *
-     * @author 하석형
-     * @since 2023.08.21
-     */
     @Bean(name = "mainMapperScannerConfigurer")
     public MapperScannerConfigurer getMainMapperScannerConfigurer () {
         MapperScannerConfigurer mapperScannerConfigurer = null;
@@ -88,13 +64,6 @@ public class RootContextConfig {
         return mapperScannerConfigurer;
     }
 
-    /**
-     * Main
-     * Mybatis SqlSessionFactory Bean 설정
-     *
-     * @author 하석형
-     * @since 2023.08.21
-     */
     @Bean(name = "mainSqlSessionFactory")
     public SqlSessionFactory getMainSqlSessionFactory (ApplicationContext applicationContext) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
@@ -104,25 +73,12 @@ public class RootContextConfig {
         return sqlSessionFactoryBean.getObject();
     }
 
-    /**
-     * Main
-     * Mybatis SqlSessionTemplate Bean 설정
-     *
-     * @author 하석형
-     * @since 2023.08.21
-     */
     @Bean(name = "mainSqlSessionTemplate")
     public SqlSessionTemplate getMainSqlSessionTemplate (ApplicationContext applicationContext) throws Exception {
         SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(getMainSqlSessionFactory(applicationContext));
         return sqlSessionTemplate;
     }
 
-    /**
-     * JSON Parser 라이브러리 Class Bean 설정
-     *
-     * @author 하석형
-     * @since 2023.08.24
-     */
     @Bean(name = "objectMapper")
     public ObjectMapper getObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
@@ -134,14 +90,6 @@ public class RootContextConfig {
         mapper.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
         return mapper;
     }
-
-    /**
-     * JSON ViewResolver를 제공해 주기 위한 Bean
-     *
-     * @author 하석형
-     * @since 2023.08.21
-     * @return MappingJackson2JsonView
-     */
     @Bean(name="jsonView")
     public MappingJackson2JsonView getJsonView () {
         ObjectMapper objectMapper = getObjectMapper();
--- edu-test-back-end/server/main/java/spring/config/WebContextConfig.java
+++ edu-test-back-end/server/main/java/spring/config/WebContextConfig.java
@@ -12,27 +12,12 @@ import org.springframework.web.servlet.view.BeanNameViewResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = {"kr.co.edu"})
 public class WebContextConfig implements WebMvcConfigurer {
-
-    /**
-     * 1. 특정 URL(Path)에 대한 HTTP Request 처리를 DispatcherServlet이 담당하지 않도록 만들어 주는 설정 (html, js, css, file 등..)
-     * 2. Client가 접근하지 못하는 WEB-INF 폴더안에 위치해 있는 정적 자원에 Clien가 접근할 수 있도록 만들어 주는 설정
-     *
-     * @author 최정우
-     * @since 2022.08.31
-     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
         registry.addResourceHandler("/views/**").addResourceLocations("/views/");
     }
 
-    /**
-     * Custom Bean Class ViewResolver를 제공해 주기 위한 Bean (파일 다운로드, JSON 등..)
-     *
-     * @author 최정우
-     * @since 2022.08.31
-     * @return BeanNameViewResolver
-     */
     @Bean(name="beanView")
     public BeanNameViewResolver getBeanView () {
         BeanNameViewResolver beanView = new BeanNameViewResolver();
